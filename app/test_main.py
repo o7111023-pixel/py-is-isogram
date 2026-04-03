9282d508f57781ce6051fb755ab90870cd21e949
Oleh M <oleh.murachov28@gmail.com>
2026-04-03 18:21:56 +0200

    gov

--- app/test_main.py
+++ app/test_main.py
@@ -12,7 +12,7 @@ from app.main import is_isogram
     ("aA", False),
     ("abcdeFghIjklmnOpqrStuvWxYz", True),
 ])
-def test_is_isogram(word, expected):
+def test_is_isogram(word, expected) -> None:
     assert is_isogram(word) == expected
 
 
